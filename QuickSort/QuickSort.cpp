c441627beba88f83ce55f9574053f50c50955cf8
raditanurhasna <raditanurhasna1410@gmail.com>
2023-03-30 10:57:57 +0700

    Memasukan array bilangan bulat

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // array of integers to hold values
-int arr[20];
+int arr[20]; 
 int cmp_count = 0; // number of comparasion
 int mov_count = 0; // number of data movements
 int n;

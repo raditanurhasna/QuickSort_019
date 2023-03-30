8568e5ed5fa962b10e066c650dfa63deaa288cf6
raditanurhasna <raditanurhasna1410@gmail.com>
2023-03-30 11:45:57 +0700

    Membuat Tampilan Output dari Array yang diurutkan

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -97,4 +97,19 @@ void q_short(int low, int high)
 }
 
 
+void display() {
+	cout << "\n--------------------" << endl;
+	cout << "sorted Array" << endl;
+	cout << "----------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+	cout << "Number of data movements: " << mov_count << endl;
+}
+
+
 

a5f97e63edb6dd57ff49028519bf6b05eccbca0e
raditanurhasna <raditanurhasna1410@gmail.com>
2023-03-30 10:54:48 +0700

    Memasukan Panjang Array

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 
-
+// array of integers to hold values
 int arr[20];
-int cmp_count = 0;
-int mov_count = 0;
+int cmp_count = 0; // number of comparasion
+int mov_count = 0; // number of data movements
 int n;
 
 
@@ -15,9 +15,21 @@ void input() {
 		cin >> n;
 
 		if (n <= 20)
-			break
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-------------------" << endl;
+
+	for (int i = 0; i < n; i++) 
+	{
+		cout << "<" << (i + 1) << ">";
+
 	}
 }
+
 
 
 

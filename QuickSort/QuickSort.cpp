cf8e179fab0ad4d9c27160cd808193dc2494a26c
raditanurhasna <raditanurhasna1410@gmail.com>
2023-03-30 10:33:59 +0700

    Add project files.

---
+++ QuickSort/QuickSort.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int arr[20];
+int cmp_count = 0;
+int mov_count = 0;
+int n;
+
+
+void input() {
+	while (true)
+	{
+		cout << "Masukan Panjang element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break
+	}
+}
+
+
+
